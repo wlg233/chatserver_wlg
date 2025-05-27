650aaf0a93c821d0a5a856eb3332138e31aa5099
wlg233 <3207565398@qq.com>
2025-05-27 17:58:03 +0800

    更新客户端main.cpp

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wlg233/workcpp/project1/chatserver/src/server/chatserver.cpp" "/home/wlg233/workcpp/project1/chatserver/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/home/wlg233/workcpp/project1/chatserver/src/server/chatservice.cpp" "/home/wlg233/workcpp/project1/chatserver/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/home/wlg233/workcpp/project1/chatserver/src/server/db/db.cpp" "/home/wlg233/workcpp/project1/chatserver/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/home/wlg233/workcpp/project1/chatserver/src/server/main.cpp" "/home/wlg233/workcpp/project1/chatserver/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/wlg233/workcpp/project1/chatserver/src/server/model/friendmodel.cpp" "/home/wlg233/workcpp/project1/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
+  "/home/wlg233/workcpp/project1/chatserver/src/server/model/groupmodel.cpp" "/home/wlg233/workcpp/project1/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/home/wlg233/workcpp/project1/chatserver/src/server/model/offlinemessagemodel.cpp" "/home/wlg233/workcpp/project1/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/home/wlg233/workcpp/project1/chatserver/src/server/model/usermodel.cpp" "/home/wlg233/workcpp/project1/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/home/wlg233/workcpp/project1/chatserver/src/server/redis/redis.cpp" "/home/wlg233/workcpp/project1/chatserver/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
